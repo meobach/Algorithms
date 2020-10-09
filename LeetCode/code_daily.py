d404483b521c6f58276389df251f2c15260b763c
Bach Thien Quoc Minh Thong <thongbtqm@Bachs-iMac.local>
2020-10-09 16:57:42 +0700

    add solution for two sum

---
+++ LeetCode/code_daily.py
@@ -0,0 +1,32 @@
+#def twoSum(self, nums, target):
+def twoSum(nums, target):
+
+    if(target%2==0):
+        count=0
+        arr=[]
+        half_target=int(target/2)
+        for i,item in enumerate(nums):
+            if(item==half_target):
+                count+=1
+                arr.append(i)
+        if(count==2):
+            return arr
+    index_list=dict()
+    for i,item in enumerate(nums):
+        index_list[item]=i
+    sorted_nums=sorted(nums)
+    i,j=0,len(sorted_nums)-1
+    total=sorted_nums[i]+sorted_nums[j]
+    while(True):
+        if(total<target):
+            total-=sorted_nums[i]
+            i+=1
+            total+=sorted_nums[i]
+        else:
+            if(total>target):
+                total-=sorted_nums[j]
+                j-=1
+                total+=sorted_nums[j]
+            else:
+                return [index_list[sorted_nums[i]],index_list[sorted_nums[j]]]
+print(twoSum([3,2,4],6))
