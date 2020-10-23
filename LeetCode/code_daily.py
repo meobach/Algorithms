d8f82423e53fe22cf6b9ce047d9b576443285480
Bach Thien Quoc Minh Thong <thongbtqm@Bachs-iMac.local>
2020-10-23 15:25:58 +0700

    add image

--- LeetCode/code_daily.py
+++ LeetCode/code_daily.py
@@ -30,3 +30,17 @@ def twoSum(nums, target):
             else:
                 return [index_list[sorted_nums[i]],index_list[sorted_nums[j]]]
 print(twoSum([3,2,4],6))
+
+def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]
